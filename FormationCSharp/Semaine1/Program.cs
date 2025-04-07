d2a8a329ac9373a49193f76c807e5092c2b4bbf5
collomsu <collomb.sullivan@gmail.com>
2025-04-07 14:37:16 +0200

    Serie 1 : Ajout des 3 méthodes de l'exercice 1

--- FormationCSharp/Semaine1/Program.cs
+++ FormationCSharp/Semaine1/Program.cs
@@ -10,7 +10,103 @@ namespace Semaine1
     {
         static void Main(string[] args)
         {
+            BasicOperation(1, 2, '+');
+            BasicOperation(1, 2, 'L');
+            BasicOperation(1, 0, '/');
 
+            IntegerDivision(8, 2);
+            IntegerDivision(5, 2);
+            IntegerDivision(8, 0);
+        }
+
+        // Exercice 1
+
+        /// <summary>
+        /// Réalisé l'opération donné par le caractère op entre deux valeurs a et b données.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="op"></param>
+        static void BasicOperation(int a, int b, char op)
+        {
+            string res;
+
+            switch(op)
+            {
+                case '+':
+                    res = (a + b).ToString();
+                    break;
+                case '-':
+                    res = (a - b).ToString();
+                    break;
+                case '*':
+                    res = (a * b).ToString();
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        res = (a / b).ToString();
+                    }
+                    else
+                    {
+                        res = "Opération invalide.";
+                    }
+                    break;
+                default:
+                    res = "Opération invalide.";
+                    break;
+            }
+            Console.WriteLine($"{a} {op} {b} = {res}");
+        }
+
+        /// <summary>
+        /// Effectue la division non nul entre deux entiers a et b donnés.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void IntegerDivision(int a, int b)
+        {
+            int r = a;
+            int q = 0;
+
+            if (b != 0)
+            {
+                while (r >= b)
+                {
+                    r -= b;
+                    q++;
+                }
+
+                if (r > 0)
+                {
+                    Console.WriteLine($"{a} = {q} * {b} + {r}");
+                }
+                else
+                {
+                    Console.WriteLine($"{a} = {q} * {b}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{a} : {b} = Opération invalide");
+            }
+        }
+
+        /// <summary>
+        /// Affiche le calcul de l'entier a à la puissance b.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Pow(int a, int b) 
+        {
+            if (b >= 0)
+            {
+                Console.WriteLine($"{a} ^ {b} = {Math.Pow(a,b)}");
+            }
+            else
+            {
+                Console.WriteLine("Opération invalide");
+            }
         }
     }
 }
